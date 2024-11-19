aa85b20827b5752e036428e404156088a28d5792
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-11-20 02:41:04 +0530

    toggle theme

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -10,14 +10,22 @@ export function ThemeToggle() {
   const { setTheme } = useTheme()
 
   return (
-        <Button variant="outline" className="mx-auto" size="sm"
-        onClick={
-          () => {
-            setTheme((oldTheme) => (oldTheme === "light" ? "dark" : "light"))
-          }
-        }>
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        </Button>
+    <div
+      className={`
+        fixed bottom-2 right-4 p-2 z-10
+      `}
+    >
+      <Button
+        variant="outline"
+        className="mx-auto rounded-full"
+        size="sm"
+        onClick={() => {
+          setTheme((oldTheme) => (oldTheme === "light" ? "dark" : "light"))
+        }}
+      >
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      </Button>
+    </div>
   )
 }
