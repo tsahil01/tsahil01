aa379c62a5bebbb80d25db9572cd08f7db355785
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-11-20 00:08:16 +0530

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import { About } from "@/components/about";
+import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Projects } from "@/components/projects";
 import { Skills } from "@/components/skills";
 
 export default function Home() {
   return (
-    <div className="flex flex-col mt-4 max-w-3xl mx-auto p-5 gap-7">
+    <>
+    <div className="flex flex-col mt-4 max-w-3xl mx-auto p-5 gap-7 border-b">
       <Header />
       <About />
       <Projects />
       <Skills />
-    </div>);
+    </div>
+      <Footer />
+    </>
+    );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+    return <>
+    <div className="mt-8 p-5">
+        <p className="md:text-base text-sm text-primary/60 italic text-center">
+            Liked my work? Want to collaborate? Feel free to reach out to me on <a href="https://twitter.com/tsahil01" className="text-blue-500">Twitter</a> or <a href=" https://www.linkedin.com/in/sahiltiwaskar/" className="text-blue-500">LinkedIn</a> 
+        </p>
+
+    </div>
+    </>
+}
