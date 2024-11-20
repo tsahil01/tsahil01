3f764f6c59e2aad3fc8d979b3fc746136761d21a
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-11-20 13:42:40 +0530

    fix footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
     return <>
     <div className="mt-8 p-5 flex flex-row max-w-3xl mx-auto gap-2">
         <p className="md:text-base text-sm text-primary/60 italic text-center my-auto">
-            Liked my work? Want to collaborate? Feel free to reach out to me on <a href="https://twitter.com/tsahil01" className="text-blue-500">Twitter</a> or <a href=" https://www.linkedin.com/in/sahiltiwaskar/" className="text-blue-500">LinkedIn</a> 
+            Liked my work? Want to collaborate? Feel free to reach out to me on <a href="https://twitter.com/t__sahil" className="text-blue-500">Twitter</a> or <a href="https://www.linkedin.com/in/sahil-tiwaskar/" className="text-blue-500">LinkedIn</a> 
         </p>
         <ThemeToggle/>
     </div>
