004ee6d2b2db1996196162418ae815f312c8a4e2
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-06-21 11:18:21 +0530

    feat: update footer links to X and email for better accessibility

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,10 +3,9 @@ import { ThemeToggle } from "./ToggleTheme";
 export function Footer() {
     return <>
     <div className="mt-8 p-5 flex flex-row max-w-3xl mx-auto gap-2">
-        <p className="md:text-base text-sm text-primary/60 italic text-center my-auto">
-            Liked my work? Want to collaborate? Feel free to reach out to me on <a href="https://twitter.com/t__sahil" className="text-blue-500">Twitter</a> or <a href="https://www.linkedin.com/in/sahil-tiwaskar/" className="text-blue-500">LinkedIn</a> 
+        <p className="text-sm text-primary/60 italic text-center my-auto">
+            Liked my work? Want to collaborate? Feel free to reach out to me on <a href="https://x.com/t__sahil" className="text-blue-500">X</a> or <a href="mailto:sahiltiwaskar2003@gmail.com" className="text-blue-500">Email</a> 
         </p>
-        <ThemeToggle/>
     </div>
     </>
 }
